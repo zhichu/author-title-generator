6e1db056b1ae94e1eb6bc94487feab88962d041e
ℤhichu ℂhen <1871526+zhichu@users.noreply.github.com>
2023-09-07 01:58:21 +0800

    concatenates superscript texts together
    Add a new LaTeX type
    YAML -> AuthorTitle -> [LaTeX] -> String
    Adding a new type enables the program to manipulate the objects.

--- JACoW/Types.hs
+++ JACoW/Types.hs
@@ -4,50 +4,69 @@ module JACoW.Types where
 
 import Data.Yaml
 import Control.Applicative
+import Data.List
 import Data.Maybe
-
-import Data.Text hiding
-       (
-         zip,foldl1,
-       )
-
-import Data.List hiding
-       (
-         unlines,
-         intercalate,singleton,
-         unwords,
-       )
+import Data.Char
 
 import Prelude hiding
        (
-         unlines,
-         unwords,
+         
        )
 
 class LaTeXable a where
-    totitle :: a -> [Affiliation] -> Text
+    fromtitle :: a -> [Affiliation] -> [LaTeX]
+    tolatex   :: a -> [LaTeX]
 
-simpletitle :: (LaTeXable a) => a -> Text
-simpletitle = flip totitle []
-cs1 :: Text -> Text -> Text
-cs1 cs arg1 = "\\" <> cs <> "{" <> arg1 <> "}"
+stringify :: LaTeX -> String
+
+stringify (Raw s) = s
+stringify (Sup s) = supTeX s
+stringify (Maþ s) = mathTeX s
+stringify (Mac c ls) = cs1 c $ foldl1 (<>) $ stringify <$> ls
+stringify Spc = " "
+stringify NewLine = "\n"
+stringify TeXLine = "\\\\"
+cs1 :: String -> String -> String
+cs1 cs arg1 = "\\" <> cs <> wrap "{" "}" arg1
+wrap :: String -> String -> String -> String
+wrap l r s = l <> s <> r
+supTeX    = cs1 "textsuperscript"
+mathTeX   = wrap "$" "$"
+-- sep :: String -> [LaTeX] -> String
+-- sep s ltxs = foldl1 (<> s <>) (fmap toLaTeX ltxs)
+simplify :: [LaTeX] -> [LaTeX]
+
+simplify [] = []
+simplify (Mac cs ls:xs) = (Mac cs (simplify ls)):simplify xs
+simplify (x:[]) = [x]
+simplify (Raw x1:Raw x2:xs) = simplify ((Raw (x1<>x2)):xs)
+simplify (Sup x1:Sup x2:xs) = simplify ((Sup (x1<>x2)):xs)
+simplify (Maþ x1:Maþ x2:xs) = simplify ((Maþ (x1<>x2)):xs)
+simplify (x:xs) = x:simplify xs
 authorTeX  = cs1 "author"
 unTeXlines = intercalate ("\\\\" <> singleton '\n')
-titleTeX  = cs1 "title"
-thanksTeX = cs1 "thanks"
-supTeX    = cs1 "textsuperscript"
-supComma  = supTeX ","
-sep       = intercalate supComma
-maybePrimary :: Maybe Text -> Bool
-maybePrimary (Just str) = (== "primary").toLower $ str
+infixl 4 <<$>>
+(<<$>>) :: (Functor f, Functor g) => (a -> b) -> f (g a) -> f (g b)
+(<<$>>) = fmap . fmap
+maybePrimary :: Maybe String -> Bool
+maybePrimary (Just str) = (== "primary") $ toLower <$> str
 maybePrimary _ = False
-addrIndex :: Affiliation -> [Affiliation] -> Maybe Text
-addrIndex aff affs = (supTeX.pack.show.(+1)) <$> aff `elemIndex` affs
+addrIndex :: Affiliation -> [Affiliation] -> Maybe LaTeX
+addrIndex aff affs = ((Sup).show.(+1)) <$> aff `elemIndex` affs
 
+data LaTeX = Raw String
+           | Sup String
+           | Maþ String
+           | Mac String [LaTeX]
+           | Spc
+           | NewLine
+           | TeXLine
+           deriving Show
 data AuthorTitle = AuthorTitle
                    { title   :: Title
                    , authors :: [Author]
                    } deriving (Show)
+emptyAuthorTitle = AuthorTitle (Title "" Nothing) [Author (Name "" "" Nothing) "" Nothing False]
 instance FromJSON AuthorTitle where
   parseJSON (Object v) =
     AuthorTitle    <$>
@@ -55,16 +74,19 @@ instance FromJSON AuthorTitle where
     v .:   "authors"
   parseJSON _ = fail "Expected Object for Config value"
 instance LaTeXable AuthorTitle where
-  totitle (AuthorTitle t as) _  = unlines [totitle t affs, authorTeX (unTeXlines ([unlines auths] <> fmap toAff (zip [1..] affs)))]
+  tolatex (AuthorTitle t as)  = intercalate [NewLine] [tolatex t,[Mac "author" (intercalate [TeXLine,NewLine] ([authorlist] <> affiliations ))]]
     where
-      affs = fromMaybe [] (nub <$> (foldl1 (<>) $ fmap addresses as))
-      auths = fmap (flip totitle affs) as
-      toAff (n,aff) = supTeX ((pack.show) n) <> totitle aff affs
+      authorlist = intercalate [NewLine] authors
+      authors = flip fromtitle affs <$> as
+      affs = fromMaybe [] (nub <$> (foldl1 (<>) $ addresses <$> as))
+      affiliations = toAff <$> (zip [1..] affs)
+      toAff (n,aff) = [Sup (show n)] <> fromtitle aff affs
+  fromtitle t _ = tolatex t
 data Title = Title
-             { caption :: Text
+             { caption :: String
              , funds   :: Maybe [Fund]
              } deriving (Show)
-type Fund = Text
+type Fund = String
 instance FromJSON Title where
   parseJSON (Object v) =
     Title            <$>
@@ -72,7 +94,8 @@ instance FromJSON Title where
     v .:?  "funds"
   parseJSON _ = fail "Expected Object for Config value"
 instance LaTeXable Title where
-  totitle (Title cap fs) _  = titleTeX $ cap <> sep (fromMaybe [] $ fmap (fmap thanksTeX) fs)
+  tolatex (Title cap fs)  = [Mac "title" $ [Raw cap] <> intersperse (Sup ",") (fromMaybe [] $ ((Mac "thanks").singleton.(Raw)) <<$>> fs)] --((ref:sup-comma-in-title))
+  fromtitle t _ = tolatex t
 data Author = Author
               { name       :: Name
               , email      :: Email
@@ -85,21 +108,23 @@ instance FromJSON Author where
     v .:   "name"      <*>
     v .:   "email"     <*>
     v .:?  "addresses" <*>
-    (maybePrimary <$> (v .:? "type" .!= Just "other" :: Parser (Maybe Text)))
+    (maybePrimary <$> (v .:? "type" .!= Just "other" :: Parser (Maybe String)))
   parseJSON _ = fail "Expected Object for Config value"
 instance LaTeXable Author where
-  totitle (Author nm em addrs isP) affs = totitle nm affs <> sep (thankslst <> catMaybes (fmap (flip addrIndex affs) (fromMaybe [] addrs)))
+  fromtitle (Author nm em addrs isP) affs = fromtitle nm affs <> intersperse (Sup ",") (thankslst <> catMaybes (fmap (flip addrIndex affs) (fromMaybe [] addrs))) --((ref:sup-comma-in-author))
     where thankslst =
             if isP
-            then [totitle em affs]
+            then tolatex em
             else []
-type Email = Text
+  tolatex as = fromtitle as []
+type Email = String
 instance LaTeXable Email where
-  totitle eml _ = thanksTeX eml
+  tolatex eml = [Mac "thanks" [Raw eml]]
+  fromtitle e _ = tolatex e
 data Name = Name
-            { firstName :: Text
-            , lastName  :: Text
-            , initials  :: Maybe Text
+            { firstName :: String
+            , lastName  :: String
+            , initials  :: Maybe String
             } deriving (Show)
 instance FromJSON Name where
   parseJSON (Object v) =
@@ -109,11 +134,12 @@ instance FromJSON Name where
     v .:?  "initials"
   parseJSON _ = fail "Expected Object for Config value"
 instance LaTeXable Name where
-    totitle (Name fnm lnm ini) _ = unwords [fnm,lnm] -- firstname lastname
+  tolatex (Name fnm lnm ini) = [Raw (unwords [fnm,lnm])] -- firstname lastname
+  fromtitle n _ = tolatex n
 data Affiliation = Affiliation
-                   { affName  :: Text
-                   , adress   :: Maybe Text
-                   , zipcode  :: Maybe Text
+                   { affName  :: String
+                   , adress   :: Maybe String
+                   , zipcode  :: Maybe String
                    } deriving (Show,Eq)
 instance FromJSON Affiliation where
   parseJSON (Object v) =
@@ -123,6 +149,8 @@ instance FromJSON Affiliation where
     v .:?  "zip"
   parseJSON _ = fail "Expected Object for Config value"
 instance LaTeXable Affiliation where
-  totitle aff _ = affName aff                                                    -- name
-                  <> fromMaybe "" (((<>) ", ") <$> adress aff)                   -- , address
-                  <> fromMaybe "" (((<>) ", ") <$> zipcode aff)                  -- , zip
+  tolatex aff = [Raw (affName aff                                                    -- name
+                      <> fromMaybe "" (((<>) ", ") <$> adress aff)                   -- , address
+                      <> fromMaybe "" (((<>) ", ") <$> zipcode aff)                  -- , zip
+                     )]
+  fromtitle aff _ = tolatex aff
--- yml2tex.hs
+++ yml2tex.hs
@@ -2,10 +2,12 @@ module Main where
 
 import JACoW.Types
 import qualified Data.Yaml as Y
-import qualified Data.Text.IO as TIO
+--import qualified Data.Text.IO as TIO
 import qualified Data.ByteString.Char8 as BS
+
+simpletitle at = foldl1 (<>) $ stringify <$> (simplify $ tolatex at)
 
 main = do
   ymlData <- BS.readFile "mypaper.yaml"
   let authortitle = Y.decodeEither' ymlData :: Either Y.ParseException AuthorTitle
-  TIO.writeFile "mypaper-hs.tex" $ either (error . show) simpletitle authortitle
+  writeFile "mypaper-hs.tex" $ either (error . show) simpletitle authortitle
